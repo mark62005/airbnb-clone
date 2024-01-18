3ad78b1637aeca83e2bab44c51d74b4c7e29ee7a
Mark Wong <markwong.digital@gmail.com>
2024-01-17 17:39:27 -0800

    Initial commit

---
+++ assets/data/places.ts
@@ -0,0 +1,26 @@
+export const places = [
+	{
+		title: "I'm flexible",
+		img: require("@/assets/images/world-0.png"),
+	},
+	{
+		title: "United States",
+		img: require("@/assets/images/world-1.png"),
+	},
+	{
+		title: "Italy",
+		img: require("@/assets/images/world-2.png"),
+	},
+	{
+		title: "Middle East",
+		img: require("@/assets/images/world-3.png"),
+	},
+	{
+		title: "Netherlands",
+		img: require("@/assets/images/world-4.png"),
+	},
+	{
+		title: "Southeast Asia",
+		img: require("@/assets/images/world-5.png"),
+	},
+];
